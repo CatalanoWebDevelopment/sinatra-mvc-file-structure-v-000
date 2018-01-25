deecce8e7f1aa86e10936e4b7533f8f6ece175e9
Parker Catalano <catalano.work@gmail.com>
2018-01-25 18:02:39 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_accessor :name, :breed, :age
   @@all = []
 
-  def inititalize(name, breed, age)
+  def inititalize(name = nil, breed, age)
     @name = name
     @breed = breed
     @age = age
